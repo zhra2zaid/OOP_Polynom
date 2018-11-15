4659b3fc2320aa850a5872c63fab82e8c313589c
zhra2zaid <44701354+zhra2zaid@users.noreply.github.com>
2018-11-15 15:22:51 +0200

    Update Monom.java

--- myMath/Monom.java
+++ myMath/Monom.java
@@ -45,6 +45,10 @@ public class Monom implements function{
 			try {
 				throw new Exception("The input isn't good");
 			} catch (Exception e) {e.printStackTrace();	}}
+		else if((!input.contains("*"))   && (input.contains("x"))&& (input.length()>1)  && (input.charAt(0)!='x') && (input.charAt(0)!='-')) {
+			try {
+				throw new Exception("The input isn't good");
+			} catch (Exception e) {e.printStackTrace();	}}
 		
 		String[] Monom_arr;
 		if((input.contains("*") && input.contains("x"))  ) {
